932fe2d9261a04c6b60c8d093185615f65d1e4aa
Josh Richards <jmr8776@rit.edu>
2018-06-24 14:09:34 -0400

    Added Back to Top button

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.parallax').parallax();
-    $("html").easeScroll();
+    $('html').easeScroll({
+        frameRate: 60,
+        animationTime: 1000,
+        stepSize: 120,
+        pulseAlgorithm: 1,
+        pulseScale: 8,
+        pulseNormalize: 1,
+        accelerationDelta: 20,
+        accelerationMax: 1,
+        keyboardSupport: true,
+        arrowScroll: 50,
+        touchpadSupport: true,
+        fixedBackground: true
+    });
+    $('.backToTop').click(function() {
+        window.scrollTo({
+            top: 0, 
+            left: 0, 
+            behavior: 'smooth' 
+        });
+    });
 });
